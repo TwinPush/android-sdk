4f2577da2d7f2892a93ce9790ef63dd41ad763ab
Alex Gutiérrez <alejandro.gutierrez@twincoders.com>
2015-02-17 11:18:41 +0100

    Updated big offer link

--- mobile/src/main/java/com/yellowpineapple/offers101/activities/OffersActivity.java
+++ mobile/src/main/java/com/yellowpineapple/offers101/activities/OffersActivity.java
@@ -67,7 +67,7 @@ public class OffersActivity extends OfferListActivity {
 
     @Click(R.id.btnBigOffer)
     void bigOfferPressed() {
-        String url = "http://www.renault.es/gama-renault/renault-vehiculos-turismos/gama-kadjar/kadjar/";
+        String url = "http://goo.gl/Jhe9re";
         WebViewActivity_.intent(this).url(url).titleId(R.string.big_offer).start();
         slideInTransition();
     }
