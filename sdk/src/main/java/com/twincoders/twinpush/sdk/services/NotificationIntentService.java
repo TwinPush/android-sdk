0d543d28b570a82387dac931d9af0cb3c7b41d09
Álex Gutiérrez <alejandro.gutierrez@twincoders.com>
2026-04-29 19:34:53 +0200

    Fixes indentation whitespaces

--- sdk/src/main/java/com/twincoders/twinpush/sdk/services/NotificationIntentService.java
+++ sdk/src/main/java/com/twincoders/twinpush/sdk/services/NotificationIntentService.java
@@ -22,23 +22,23 @@ public class NotificationIntentService extends FirebaseMessagingService implemen
 	public final static String PROPERTY_CHANNEL_ID = "tp_channel_id";
 
 	protected final DefaultNotificationService defaultService = new DefaultNotificationService();
-	
-    public NotificationIntentService() {
-        super();
-    }
+
+	public NotificationIntentService() {
+		super();
+	}
 
 	@Override
 	public void onMessageReceived(RemoteMessage message){
-    	Ln.i("Received message");
+		Ln.i("Received message");
 
 		// Check if message contains a notification payload.
 		if (message.getNotification() != null) {
 			super.onMessageReceived(message);
 		} else {
 			TwinPushSDK twinpush = TwinPushSDK.getInstance(this);
-            // Ensure default channel creation to avoid issues on recently updated Android 8 devices
+			// Ensure default channel creation to avoid issues on recently updated Android 8 devices
 			twinpush.createNotificationChannel();
-            // Obtain Push Notification object from message data
+			// Obtain Push Notification object from message data
 			PushNotification notification = getNotification(message.getData());
 			// Send push notification acknowledgement if enabled
 			if (twinpush.isPushAckEnabled() && NotificationManagerCompat.from(getBaseContext()).areNotificationsEnabled())
@@ -50,28 +50,28 @@ public class NotificationIntentService extends FirebaseMessagingService implemen
 				displayNotification(getBaseContext(), notification);
 			}
 		}
-    }
-
-    @Override
-    public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
-        Ln.d("TwinPush on new token called");
-
-        TwinPushSDK twinPush = TwinPushSDK.getInstance(getApplicationContext());
-
-        // Get updated InstanceID token.
-        Ln.d("FCM Token created: " + s);
-
-        // Refresh registration if needed
-        if (twinPush.isDeviceRegistered()) {
-            twinPush.register();
-        }
-    }
-
-    // Push receiver service implementation
+	}
 
 	@Override
-    public void displayNotification(Context context, PushNotification notification) {
+	public void onNewToken(@NonNull String s) {
+		super.onNewToken(s);
+		Ln.d("TwinPush on new token called");
+
+		TwinPushSDK twinPush = TwinPushSDK.getInstance(getApplicationContext());
+
+		// Get updated InstanceID token.
+		Ln.d("FCM Token created: " + s);
+
+		// Refresh registration if needed
+		if (twinPush.isDeviceRegistered()) {
+			twinPush.register();
+		}
+	}
+
+	// Push receiver service implementation
+
+	@Override
+	public void displayNotification(Context context, PushNotification notification) {
 		defaultService.displayNotification(context, notification, getContentIntent(context, notification));
 	}
 
@@ -87,6 +87,6 @@ public class NotificationIntentService extends FirebaseMessagingService implemen
 
 	@Override
 	public void onSilentPushReceived(Context context, PushNotification notification) {
-    	defaultService.onSilentPushReceived(context, notification);
+		defaultService.onSilentPushReceived(context, notification);
 	}
 }
